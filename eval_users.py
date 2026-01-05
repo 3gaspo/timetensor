f18448c91cbf69d8cf573765ae7e592c47c55cd5
Gaspard Berthelier <gaspard.berthelier@edf.fr>
2026-01-05 17:12:17 +0100

    test

--- eval_users.py
+++ eval_users.py
@@ -17,7 +17,7 @@ from src.timetensor.utils import symlog
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
+#test
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def run(cfg):
     logger = logging.getLogger(__name__)
